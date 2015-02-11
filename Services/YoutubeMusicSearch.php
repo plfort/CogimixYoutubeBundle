57d6d9c3aed9182f75d97664ecec54ab30f9494f
plfort <plfort@cogipix.com>
2015-02-11 21:03:00 +0100

    Fix popular query

--- Services/YoutubeMusicSearch.php
+++ Services/YoutubeMusicSearch.php
@@ -41,31 +41,34 @@ class YoutubeMusicSearch extends AbstractMusicSearch
             $this->logger->err($ex);
             return array();
         }
-        
+
         return $this->parseResponse($feeds);
     }
 
     protected function executePopularQuery()
     {
+
         $videoQuery = new \Zend_Gdata_YouTube_VideoQuery();
-        
-        $videoQuery->setCategory('music');
+
+        $videoQuery->setCategory('Music');
         $videoQuery->setFeedType('most viewed');
+        $videoQuery->setMaxResults(50);
+
         $this->logger->info('Youtube executePopularQuery');
         try {
-            $feeds = $this->youtubeService->getVideoFeed($videoQuery);
+            $feeds = $this->youtubeService->getMostViewedVideoFeed($videoQuery);
         } catch (\Exception $ex) {
             $this->logger->err($ex);
             return array();
         }
-        
+
         return $this->parseResponse($feeds);
     }
 
     protected function buildQuery()
     {
         $this->videoQuery = new \Zend_Gdata_YouTube_VideoQuery();
-        
+
         //$this->videoQuery->setCategory('music');
         $this->videoQuery->setOrderBy('relevance');
         $this->videoQuery->setVideoQuery($this->searchQuery->getSongQuery());
